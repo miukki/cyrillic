b40213b8c36468290ea1f86254b2aa95e92cb568
Anja Ishmukhametova <miukki@gmail.com>
2015-07-02 19:29:03 +0800

    add highlight for keyboard

--- scripts/cyrillic.js
+++ scripts/cyrillic.js
@@ -1,7 +1,7 @@
 +function ($) {
   'use strict';
 
-  var alphabet = [
+  window.alphabet  =[
     {l: 'a', c: 'а'},
     {l: 'b', c: 'б'},
     {l: 'v', c: 'в'},
@@ -28,14 +28,17 @@
     {l: 'c', c: 'ц'},
     {l: 'ch', c: 'ч'},
     {l: 'sh', c: 'ш'},
-    {l: 'shh', c: 'щ'},
-    {l: '\'\'', c: 'ъ'},
+    {l: 'w', c: 'щ'},
+    {l: '"', c: 'ъ'},
     {l: 'y', c: 'ы'},
     {l: '\'', c: 'ь'},
     {l: 'eh', c: 'э'},
     {l: 'ju', c: 'ю'},
     {l: 'ja', c: 'я'}
-  ]
+  ];
+  var reverse =  Array.prototype.sort.call(alphabet.slice(), function(a,b){if(a.l.length > b.l.length) return -1; return 0;});
+
+
 
   // CYRILLIC CLASS DEFINITION
   // =========================
@@ -83,14 +86,24 @@
     return;
   }
 
-  Cyrillic.prototype.keydown = function (e) {
+  var replaceSymbols = function(val){
+    for(var i in reverse) {
+      var item = alphabet[i];
+      var re = new RegExp(item.l + '$');
+      if (re.test(val)){
+        return [val.replace(re, item.c), item.c];
+      }
+    }
+    return [val, null]//.replace(/(h|q|w)$/,'')
+  }
+
+  Cyrillic.prototype.keyup = function (e) {
     var $this = $(this);
     var $parent  = getParent($this);
     var $helper = $parent.find(helper);
     var isActive = $parent.hasClass('active');
+    var val = $this.val();
 
-    e.preventDefault();
-    e.stopPropagation();
 
     if (/27/.test(e.which)) return $this.blur();
 
@@ -100,19 +113,27 @@
 
     if (!isActive) $this.trigger('click');
 
-    $helper.trigger('toggleKeyboard', { relatedTarget: this });
-    console.log('e.which', e.which, /13/.test(e.which) ? 'enter': '');
+    $this.val(replaceSymbols(val)[0]);
+    $helper.trigger('toggleKeyboard', { relatedTarget: this, table: $helper.find('table'), marker: replaceSymbols(val)[1] });
+
+    //console.log('e.which', e.which, /13/.test(e.which) ? 'enter': '', String.fromCharCode(e.which), '!!!', val);
 
   }
 
-  Cyrillic.prototype.toggleKeyboard = function(e){
+  Cyrillic.prototype.toggleKeyboard = function(e, data){
     var $this = $(this);
     var isActive = getParent($this).hasClass('active');
 
     if (!isActive) return;
 
     $(this).removeClass('hide');
-    console.log('do something', alphabet.length);
+
+    console.log('do something', alphabet.length, data.marker);
+    if (data.marker) {
+      var el = data.table.find('[data-marker="'+data.marker+'"]');
+      el.toggleClass('success');
+      setTimeout(function(){el.toggleClass('success');}, 500)
+    }
   }
 
   Cyrillic.prototype.blur = function(e) {
@@ -125,7 +146,6 @@
   }
 
   Cyrillic.prototype.initKeyboard = function() {
-
     console.log('Cyrillic.prototype.initKeyboard')
 
     var $this = $(this);
@@ -134,8 +154,8 @@
 
     for(var i in alphabet) {
       var item = alphabet[i];
-      trC.append($('<td></td>').html(item.c))
-      trL.append($('<td></td>').html(item.l))
+      trC.append($('<td data-marker="'+item.c+'"></td>').html(item.c));
+      trL.append($('<td data-marker="'+item.c+'"></td>').html(item.l))
     }
     $this.find('table').html('').append(trC).append(trL)
 
@@ -155,10 +175,7 @@
     .on('blur', toggle, Cyrillic.prototype.blur)
     .on('toggleKeyboard', helper, Cyrillic.prototype.toggleKeyboard )
     .on('initKeyboard', helper, Cyrillic.prototype.initKeyboard )
-    .on('keydown', toggle, Cyrillic.prototype.keydown);
-
-
-
+    .on('keyup', toggle, Cyrillic.prototype.keyup);
 
 
 
